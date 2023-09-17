b4f600a411cde86793e65f32ce9d67caac26f368
MichaelRea-NZ <58958188+MichaelRea-NZ@users.noreply.github.com>
2023-09-18 11:56:17 +1200

    Add files via upload

---
+++ GroceryItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XamarinIteration1
+{
+    public class GroceryItem
+    {
+        public string ItemText { get; set; }
+        public bool InTheBasket { get; set; }
+        public GroceryItem(string ItemText, bool InTheBasket) 
+        {
+            this.ItemText = ItemText;
+            this.InTheBasket = InTheBasket;
+        }
+        public override string ToString()
+        {
+            //Console.WriteLine("To string");
+            string result = this.ItemText;
+            //Console.WriteLine(result.GetType());
+            return result;
+        }
+    }
+}
